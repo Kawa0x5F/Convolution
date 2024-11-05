ce2f004d8d57df5ea29b8abb6f1187e64a08a845
Kawa_ <yuyux0910@gmail.com>
2024-11-05 11:44:13 +0900

    [add] 標準入力の受け取りを追加
    f32のVectorとして標準入力を受け取る関数を作成した．

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let x1 = read_vec_float32();
+    let x2 = read_vec_float32();
+
+    println!("{:?}", x1);
+    println!("{:?}", x2);
+}
+
+/**
+ * 標準入力を受け取り，それをfloat32bit型のVectorとして返す
+ */
+fn read_vec_float32() -> Vec<f32> {
+    let mut line = String::new();
+    stdin().read_line(&mut line).unwrap();
+    return line.trim().split_whitespace().map(|e| e.parse().ok().unwrap()).collect();
 }
